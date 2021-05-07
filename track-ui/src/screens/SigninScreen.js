d5eec7d842e09fe78b45f9f1d2fbef79d0725e58
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 05:01:19 +0700

    add authform and navlink component

--- track-ui/src/screens/SigninScreen.js
+++ track-ui/src/screens/SigninScreen.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { NavigationEvents } from 'react-navigation' // pass a callback function and will be automatic call
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+
+
+// onwillfocus to be called anytime that we are about to navigate to this screen
+// ondidfocus to be called anytime that we sucessfully complete the navigation over to the screen
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
-};
+    const { state, signin, clearErrorMessage } = useContext(AuthContext)
 
-const styles = StyleSheet.create({});
+    return (
+        <View style={styles.container}>
+            <NavigationEvents onWillBlur={clearErrorMessage} />
+            <AuthForm
+                headerText='Sign In to Your Account'
+                errorMessage={state.errorMessage}
+                submitButtonText='Sign In'
+                onSubmit={signin}
+            />
+            <NavLink
+                routeName='Signup'
+                text='Dont have an accout? Go to sign up'
+            />
+        </View>
+    )
+}
 
-export default SigninScreen;
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+})
+
+export default SigninScreen
