1afd7a248e0657316af79f0bcdc461e9a1e9e7e8
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 04:58:15 +0700

    base URL change every 2 hour

--- track-ui/src/api/tracker.js
+++ track-ui/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     // ngrok http 8080 to create a server
-    baseURL: 'http://8a7a493424b4.ngrok.io'
+    baseURL: 'http://21b0f20f1c3c.ngrok.io'
 })
