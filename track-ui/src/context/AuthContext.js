86e819599e406cc239f654781bcb53046626f3f4
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 00:37:00 +0700

    update

--- track-ui/src/context/AuthContext.js
+++ track-ui/src/context/AuthContext.js
@@ -1,39 +1,52 @@
+// store token of user in user's device
+import { AsyncStorage } from '@react-native-async-storage/async-storage'
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload }
         default:
             return state
     }
 }
 
-const signup = (dispatch) => {
-    return ({ email, password }) => {
-        // make api requeset to sign up with that email and password
+const signup = (dispatch) => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password })
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({ type: 'signup', payload: response.data.token })
 
-
-        // if we signed up, modify our state, and say that we are authenticated
-
-        // if sign up fails, return an error somewhere
+        navigate('TrackList')
+    } catch (err) {
+        console.log(err)
+        dispatch({
+            type: 'add_error',
+            payload: 'Something went wrong with sign up'
+        })
     }
 }
 
-const signin = (dispatch) => {
+const signin = dispatch => {
     return ({ email, password }) => {
-        // try to signin
-        // handle success by handling state
-        // handle failure by showing error message
+        // Try to signin
+        // Handle success by updating state
+        // Handle failure by showing error message (somehow)
     }
 }
 
-const signout = (dispatch) => {
+const signout = dispatch => {
     return () => {
-        // somehow signout
+        // somehow sign out!!!
     }
 }
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signup, signin, signout },
-    { isSignedIn: false }
+    { signin, signout, signup },
+    { token: null, errorMessage: '' }
 )
--- track-ui/src/screens/SignupScreen.js
+++ track-ui/src/screens/SignupScreen.js
@@ -1,22 +1,50 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useState, useContext } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from '../components/Spacer' // add some margin to elements
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext)
+
+    // get the user email and password by useState
+
+
+
+
     return (
-        <>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button
-                title='Go to Signin'
-                onPress={() => navigation.navigate('Signin')}
+        <View style={styles.container}>
+            <AuthForm
+                headerText='Sign Up for Tracker'
+                errorMessage={state.errorMessage}
+                submitButtonText='Sign Up'
+                onSubmit={signup}
             />
-            <Button
-                title='Go to Main Flow'
-                onPress={() => navigation.navigate('mainFlow')}
-            />
-        </>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>Already have an account? Sign in instead</Text>
+                </Spacer>
+            </TouchableOpacity>
+        </View>
     )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    },
+    link: {
+        color: 'blue'
+    }
+})
 
 export default SignupScreen;
