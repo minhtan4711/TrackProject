f264494354a7ed119aa9eedda60c4608e85e8b05
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 00:36:20 +0700

    navigate through react component

---
+++ track-ui/src/navigationRef.js
@@ -0,0 +1,17 @@
+// function to get access to navigator
+import { NavigationActions } from 'react-navigation'
+
+let navigator
+
+export const setNavigator = (nav) => {
+    navigator = nav
+}
+
+export const navigate = (routeName, params) => {
+    navigator.dispatch(
+        NavigationActions.navigate({
+            routeName,
+            params
+        })
+    )
+}
