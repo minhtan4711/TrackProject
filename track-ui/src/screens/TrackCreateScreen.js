cd9b06683f484e006234cad55003e49630c7ba3e
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 05:02:23 +0700

    update map and a line track user

--- track-ui/src/screens/TrackCreateScreen.js
+++ track-ui/src/screens/TrackCreateScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
+import { SafeAreaView } from 'react-navigation'
+import { requestForegroundPermissionsAsync } from 'expo-location' // ask user to track their's location or not
+import Map from '../components/Map'
+
+
 
 const TrackCreateScreen = () => {
-    return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
-};
 
-const styles = StyleSheet.create({});
+    const [err, setErr] = useState(null)
 
-export default TrackCreateScreen;
+    const startWatching = async () => {
+        try {
+            await requestForegroundPermissionsAsync()
+        } catch (err) {
+            setErr(err)
+        }
+    }
+
+    useEffect(() => {
+        startWatching()
+    }, [])
+
+    return (
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h3>Create a Track</Text>
+            <Map />
+            {err ? <Text>Please enable location services</Text> : null}
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default TrackCreateScreen
