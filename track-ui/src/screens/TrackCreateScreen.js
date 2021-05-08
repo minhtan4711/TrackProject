de13fa16264dda916e7b2ba31076fb7cbad99113
minhtan4711 <minhtan471100@gmail.com>
2021-05-08 22:39:42 +0700

    add data and rename and icon

--- track-ui/src/screens/TrackCreateScreen.js
+++ track-ui/src/screens/TrackCreateScreen.js
@@ -1,37 +1,40 @@
-import React, { useEffect, useState } from 'react'
+// import '../_mockLocation'
+import React, { useContext, useCallback } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
-import { SafeAreaView } from 'react-navigation'
-import { requestForegroundPermissionsAsync } from 'expo-location' // ask user to track their's location or not
+import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map'
+import { Context as LocationContext } from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
+
+import TrackForm from '../components/TrackForm'
+
+import { FontAwesome } from '@expo/vector-icons'
 
 
 
-const TrackCreateScreen = () => {
-
-    const [err, setErr] = useState(null)
-
-    const startWatching = async () => {
-        try {
-            await requestForegroundPermissionsAsync()
-        } catch (err) {
-            setErr(err)
-        }
-    }
-
-    useEffect(() => {
-        startWatching()
-    }, [])
+const TrackCreateScreen = ({ isFocused }) => {
+    const { state: { recording }, addLocation } = useContext(LocationContext)
+    const callback = useCallback((location) => {
+        addLocation(location, recording)
+    }, [recording]) // rebuild callback from scratch
+    const [err] = useLocation(isFocused || recording, callback)
 
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h3>Create a Track</Text>
             <Map />
             {err ? <Text>Please enable location services</Text> : null}
+            <TrackForm />
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({})
 
-export default TrackCreateScreen
+TrackCreateScreen.navigationOptions = {
+    title: 'Add Track',
+    tabBarIcon: <FontAwesome name='plus' size={20} />
+}
+
+export default withNavigationFocus(TrackCreateScreen)
